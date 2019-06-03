4691640996f143db0659742daddc05d3e4d22849
GoldSea <42212431+lamhoanghai1998@users.noreply.github.com>
2019-06-03 11:39:08 +0700

    First commit

--- job/models.py
+++ job/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class job_list(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add= True)
+class job_name(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    date = models.DateTimeField(auto_created=1)
